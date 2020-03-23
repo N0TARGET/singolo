c0ec8d3c25395ff3fac57f7c40472d76cb0582f9
n0target <prost1k@yandex.ru>
2020-03-24 00:54:14 +0300

    feat: do highlighting of navigation

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 const COLOR_WHITE = 'white';
 const COLOR_ORANGE = '#f06c64';
 
-let coloredLinks = document.querySelectorAll('.link-colored');
-let pageSections = Array.from(document.getElementsByClassName("page-section"));
+addEventListener('DOMContentLoaded', function () {
+  let coloredLinks = document.querySelectorAll('.link-colored');
+  let pageSections = Array.from(document.getElementsByClassName("page-section"));
 
-addEventListener('DOMContentLoaded', highlightMenu);
-addEventListener('scroll', highlightMenu);
+  addEventListener('scroll', highlightMenu);
 
-function clearColor(coloredLinks, color) {
-  for (let i = 0; i < coloredLinks.length; i++) {
-    coloredLinks[i].style.color = color;
-  }
-}
-
-function highlightMenu() {
-  pageSections.forEach(section => {
-    if (isElementInViewport(section)) {
-      let id = section.id;
-      let element = document.querySelector(`[href='#${id}']`);
-      if (element) {
-        clearColor(coloredLinks, COLOR_WHITE);
-        element.style.color = COLOR_ORANGE;
-      }
+  function clearColor(coloredLinks, color) {
+    for (let i = 0; i < coloredLinks.length; i++) {
+      coloredLinks[i].style.color = color;
     }
-  })
-}
+  }
 
-function isElementInViewport(el) {
-  var rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
+  function highlightMenu() {
+    pageSections.forEach(section => {
+      if (isElementInViewport(section)) {
+        let id = section.id;
+        let element = document.querySelector(`[href='#${id}']`);
+        if (element) {
+          clearColor(coloredLinks, COLOR_WHITE);
+          element.style.color = COLOR_ORANGE;
+        }
+      }
+    })
+  }
+
+  function isElementInViewport(el) {
+    let rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+});
